f33199f08a3a5ed5dac971d47a274a9b0e0a9326
mohammadalshuwaiee <shuwaiee1993@gmail.com>
2021-02-17 16:40:48 +0300

    add logo image title

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
+import image from "./image/apple.jpg";
+import css from "./App.css";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header className="App-header">
+      <div>
+        <h1 className="tilte"> My Phone Store</h1>
+        <h4 className="Description">welcome to my mobile store</h4>
+
+        <img src={image} className="image" alt="iphone-image" />
+      </div>
+    </header>
   );
 }
 
